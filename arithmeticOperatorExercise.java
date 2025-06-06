3648cc5fdecb527ac09ed007cd1d224ab990e217
JulienAutomation <polycarpejulien42@gmail.com>
2025-06-06 20:13:21 +0530

    Add files via upload
    Exercise on below pattern executed: 
    Write a Java program to do the following:
    Store marks of 3 subjects: Math, Science, and English (use int data type).
    Calculate the total marks using arithmetic operators.
    Calculate the average marks using a suitable data type.
    Print both the total and average.
    Change the data type of the average variable and note the difference.

---
+++ arithmeticOperatorExercise.java
@@ -0,0 +1,17 @@
+package ProjectSample;
+
+public class arithmeticOperatorExercise {
+
+	public static void main(String[] args) {
+	int Maths = 95;
+	int Science = 91;
+	int English = 92;
+	int TotalMarks = (Maths+Science+English);
+	
+	System.out.println("Total mark = " +(TotalMarks));
+	System.out.println("Average of Subjects using Float type = " +(TotalMarks/3f));
+	System.out.println("Average of Subjects = " +(TotalMarks/3));
+	
+	}
+
+}
