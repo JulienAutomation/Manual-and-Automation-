a4ddd599db8e533504637749967f2d536ec6782b
JulienAutomation <polycarpejulien42@gmail.com>
2025-06-11 19:19:37 +0530

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -5,9 +5,11 @@ public class ForLoop {
 	public static void main(String[] args) {
 		for(int i=1;i<=100;i++)		
 		{
-			if(i!=5 && i !=10 && i!=15 && i!=20 && i!=25 ) 
-				System.out.println(i);
+			if(i%5==0) 
+			{
 				continue;
+			}
+				System.out.println(i);
 		}
 	}
 }
